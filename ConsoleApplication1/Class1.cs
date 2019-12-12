b02d3a94ccc2e7a8ee0ad239e1f0589a3b8eb3cf
MortonPNZ <morton.pnz@gmail.com>
2019-12-12 15:15:03 +0300

    “lab3”

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public string line1, line2, line3, line4;
+
+
+        public void SetX(int x)
+        {
+            if (x >= 0)
+                this.x = x;
+            else
+                throw new Exception("Значение x не может быть отрицательным");
+        }
+        public void SetY(int y)
+        {
+            if (y >= 0)
+                this.y = y;
+            else
+                throw new Exception("Значение y не может быть отрицательным");
+        }
+        public void SetSym(String ine1, String ine2, String ine3, String ine4)
+        {
+            line1 = ine1;
+            line2 = ine1;
+            line3 = ine3;
+            line4 = ine4;
+
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(line1);
+            y = y + 1;
+            Console.SetCursorPosition(x, y);
+            Console.Write(line2);
+            y = y + 1;
+            Console.SetCursorPosition(x, y);
+            Console.Write(line3);
+            y = y + 1;
+            Console.SetCursorPosition(x, y);
+            Console.Write(line4);
+            y = y + 1;
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lab3
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            Point p = new Point();
+            p.line1 = ("|++");
+            p.line2 = ("|  +");
+            p.line3 = ("|++");
+            p.line4 = ("|");
+            Console.WriteLine("Выполнил Васильев и Дружинина. 18СН1с.");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine(" Введите где расположить рисунок. Бип-бип!");
+            Console.WriteLine(" Координата Х = ");
+            p.x = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Координата Y = ");
+            p.y = int.Parse(Console.ReadLine());
+            Console.Clear();
+            p.Draw();
+            Console.ReadKey();
+        }
+
+    }
+}
